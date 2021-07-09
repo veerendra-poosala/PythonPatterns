cf1b3527f6fb484993a4513d6ac5449f756eb6a6
veeru78866 <82511592+veeru78866@users.noreply.github.com>
2021-07-09 13:37:26 +0530

    added __init__ file

--- pythonPatterns/a.py
+++ pythonPatterns/a.py
@@ -1,9 +1,12 @@
-for rows in range(7):
-    for col in range(5):
-        if ((col == 0 or col == 4) and rows!=0) or ((rows == 0 or rows == 3 ) and (col > 0 and col < 4)):
-            print("*",end="")
-            
-        else:
-            print(end=" ")
+def a():
+    for rows in range(7):
+        for col in range(5):
+            if ((col == 0 or col == 4) and rows!=0) or ((rows == 0 or rows == 3 ) and (col > 0 and col < 4)):
+                print("*",end="")
+                
+            else:
+                print(end=" ")
 
-    print()
+        print()
+if __name__=="__main__":
+    a=a()
--- pythonPatterns/b.py
+++ pythonPatterns/b.py
@@ -1,10 +1,11 @@
-for row in range(7):
-    for col in range(5):
-        if (col ==0) or (col==4 and(row!=0 and row!=3 and row!=6)) or ((row==0 or row==3 or row==6) and (col >0 and col<4)):
-            print("*",end=(""))
-        else:
-            print(end=" ")
-    print()
+def b():
+    for row in range(7):
+        for col in range(5):
+            if (col ==0) or (col==4 and(row!=0 and row!=3 and row!=6)) or ((row==0 or row==3 or row==6) and (col >0 and col<4)):
+                print("*",end=(""))
+            else:
+                print(end=" ")
+        print()
 
 #another way of printing B:
 
@@ -29,3 +30,6 @@ for row in range(7):
         else:
             print(end=" ")
     print()
+
+if __name__=="__main__":
+    b=b()
